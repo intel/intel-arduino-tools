ebdfb0c731b8bf44f3ae2379d11e850586cd70b0
David Hunt <dave@emutex.com>
2015-06-29 13:31:52 +0100

    Added dfu-utils and libusb for linux 32-bit
    Signed-off-by: David Hunt <dave@emutex.com>

---
+++ clupload/cluploadEDU_linux.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+echo "starting download script"
+echo "Args to shell:" $*
+#
+# ARG 1: Path to lsz executable.
+# ARG 2: Elf File to download
+# ARG 3: TTY port to use.
+#
+#path may contain \ need to change all to /
+path_to_exe=$1
+fixed_path=${path_to_exe//\\/\/}
+
+LD_LIBRARY_PATH=$fixed_path/../lib/
+
+#
+tty_port_id=$3
+echo "Serial Port PORT" $com_port_id 
+
+#Download the file.
+host_file_name=$2
+bin_file_name=${host_file_name/elf/bin}
+echo "BIN FILE" $bin_file_name
+
+
+DFU=$fixed_path/dfu-util
+echo "wating for device... "
+f=`$DFU -l | grep 8087:0a99 | grep sensor_core | cut -f 1 -d ' '`
+while [ "x$f" = "x" ]
+do
+    sleep 1
+    f=`$DFU -l | grep 8087:0a99 | grep sensor_core | cut -f 1 -d ' '`
+done
+
+echo "Using dfu-util to send " $bin_file_name
+$DFU -d8087:0a99 -D $bin_file_name -v --alt 7 -R
