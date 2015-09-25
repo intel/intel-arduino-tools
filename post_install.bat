fa00c3d07b085d8dc33c75b8f47075c892bf2660
Brian Baltz <brian.a.baltz@intel.com>
2015-09-25 12:42:59 -0700

    ATLEDGE-346 post_install script must be in root of package
    Signed-off-by: Brian Baltz <brian.a.baltz@intel.com>

--- post_install.bat
+++ post_install.bat
@@ -1,10 +1,10 @@
 @echo off
 set ARGS=/A /SE /SW /SA
 if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
-  dpinst-amd64.exe %ARGS%
+  drivers\dpinst-amd64.exe %ARGS%
 ) ELSE IF "%PROCESSOR_ARCHITEW6432%" == "AMD64" (
-  dpinst-amd64.exe %ARGS%
+  drivers\dpinst-amd64.exe %ARGS%
 ) ELSE (
-  dpinst-x86.exe %ARGS%
+  drivers\dpinst-x86.exe %ARGS%
 )
 exit /b 0
