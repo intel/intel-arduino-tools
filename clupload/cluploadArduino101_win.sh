53def03d22d78a040f50cb497eea31f53f35441c
Krzysztof Sywula <krzysztof.m.sywula@intel.com>
2015-12-16 13:57:58 -0800

    Handling verbose option in upload script
    JIRA: ATLEDGE-453

--- clupload/cluploadArduino101_win.sh
+++ clupload/cluploadArduino101_win.sh
@@ -1,53 +1,79 @@
 #!/bin/sh
 
-echo "starting download script"
-echo "Args to shell:" $*
-#
-# ARG 1: Path to lsz executable.
-# ARG 2: Elf File to download
-# ARG 3: TTY port to use.
-#
-#path may contain \ need to change all to /
-dfu=${1}\\dfu-util.exe
-dfu=${dfu//\\/\/}
-dfu_cmd="$dfu -d,8087:0ABA"
-sleep=${1}\\sleep.exe
-sleep=${sleep//\\/\/}
-path_to_exe=$1
-fixed_path=${path_to_exe//\\/\/}
-tmp_dfu_output=${1}\\..\\..\\.tmp_dfu_output
+dbg_print() {
+	if [ "x$verbosity" == "xverbose" ] ; then
+		echo "$@"
+	fi
+}
 
-tty_port_id=$3
-echo "Serial Port PORT" $com_port_id 
-#echo "Using tty Port" $tty_port_id 
-#
-#echo "Sending Command String to move to download if not already in download mode"
-#echo "~sketch downloadArduino101" > $tty_port_id
-#Give the host time to stop the process and wait for download
-#sleep 1
+# find_string $string $filename
+# searches for string in a file,
+# returns "found" on success and "not_found" on failure
+find_string() {
+	while read -r; do
+		[[ $REPLY = *$1* ]] && echo "found" && return;
+	done < "$2"
+	echo "not_found"
+}
 
-#Download the file.
-host_file_name=$2
-bin_file_name=${host_file_name/elf/bin}
-echo "BIN FILE" $bin_file_name
+error_out() {
+	echo "$@"
+	exit 1
+}
 
-echo "Waiting for device... "
-COUNTER=0
-$dfu_cmd -l  >$tmp_dfu_output
-f=`findstr sensor_core $tmp_dfu_output`
-while [ "x$f" = "x" ] && [ $COUNTER -lt 10 ]
-do
-    let COUNTER=COUNTER+1
-    $sleep 1
-    $dfu_cmd -l >$tmp_dfu_output
-    f=`findstr sensor_core $tmp_dfu_output`
-done
+main() {
+	echo "Starting download script..."
 
-if [ "x$f" != "x" ] ; then
-	echo "Using dfu-util to send " $bin_file_name
-	echo $dfu_cmd -D $bin_file_name -v --alt 7 -R
-	$dfu_cmd  -D $bin_file_name -v --alt 7 -R
-	echo "Sketch will execute in about 5 seconds."
-else
-	echo "ERROR: Timed out waiting for Arduino 101."
-fi
+	# ARG 1: Path to cygwin binaries
+	# ARG 2: Elf File to download
+	# ARG 3: TTY port to use.
+	# ARG 4: quiet/verbose
+	#
+	# path may contain \ need to change all to /
+
+	cyg_path="${1//\\/\/}"
+	dfu="$cyg_path/dfu-util.exe"
+	dfu_cmd="$dfu -d,8087:0ABA"
+	sleep="$cyg_path/sleep.exe"
+	tmp_dfu_output="$cyg_path/../../.tmp_dfu_output"
+
+	# We want to download .bin file instead of provided .elf
+	host_file_name=${2//\\/\/}
+	bin_file_name=${host_file_name/.elf/.bin}
+
+	com_port="$3"
+	verbosity="$4"
+
+	dbg_print "Args to shell:" "$@"
+	dbg_print "Serial Port:" "$com_port"
+	dbg_print "BIN FILE" "$bin_file_name"
+	dbg_print "Waiting for device..."
+
+	COUNTER=0
+	$dfu_cmd -l  >"$tmp_dfu_output"
+	f=$(find_string "sensor_core" "$tmp_dfu_output")
+	while [ "x$f" == "xnot_found" ] && [ $COUNTER -lt 10 ]
+	do
+		let COUNTER=COUNTER+1
+		$sleep 1
+		$dfu_cmd -l >"$tmp_dfu_output"
+		f=$(find_string "sensor_core" "$tmp_dfu_output")
+	done
+
+	if [ "x$verbosity" == "xverbose" ] ; then
+		dfu_download="$dfu_cmd -D \"$bin_file_name\" -v --alt 7 -R"
+	else
+		dfu_download="$dfu_cmd -D \"$bin_file_name\" --alt 7 -R >/dev/null 2>&1"
+	fi
+
+	if [ "x$f" == "xfound" ] ; then
+		dbg_print "Using dfu-util to send " "$bin_file_name"
+		dbg_print "$dfu_download"
+		eval $dfu_download || error_out "ERROR: DFU transfer failed"
+		echo "SUCCESS: Sketch will execute in about 5 seconds."
+	else
+		echo "ERROR: Timed out waiting for Arduino 101 on" "$com_port"
+	fi
+}
+
+main "$@"
