7db308d155445ba07a3f36b39e363ae76a209d2e
Krzysztof Sywula <krzysztof.m.sywula@intel.com>
2015-12-18 18:06:19 -0800

    Handling verbose option in upload script
    JIRA: ATLEDGE-453

--- clupload/cluploadArduino101_osx.sh
+++ clupload/cluploadArduino101_osx.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+dbg_print() {
+    if [ "x$verbosity" == "xverbose" ]; then
+        echo "$@"
+    fi
+}
+
+error_out() {
+    echo "$@"
+    exit 1
+}
+
 setup() {
     # ARG 1: Path to directory which contains dfu-util executable
     dfu_util_dir=$1
@@ -7,13 +18,21 @@ setup() {
     payload_elf=$2
     # ARG 3: TTY port to use
     tty_port_id=$3
+    # ARG 4: quiet/verbose
+    verbosity=$4
 
     # Upload the .bin instead of .elf
-    payload_bin=${payload_elf/elf/bin}
-    echo "Payload:" $payload_bin
+    payload_bin=${payload_elf/.elf/.bin}
+    dbg_print "Payload:" $payload_bin
 
     export DYLD_LIBRARY_PATH=$dfu_util_dir:$DYLD_LIBRARY_PATH
-    DFU="$dfu_util_dir/dfu-util -d,8087:0ABA"
+    dfu_cmd="$dfu_util_dir/dfu-util -d,8087:0ABA"
+
+    if [ "x$verbosity" == "xverbose" ] ; then
+        dfu_download="$dfu_cmd -D $payload_bin -v --alt 7 -R"
+    else
+        dfu_download="$dfu_cmd -D $payload_bin --alt 7 -R >/dev/null 2>&1"
+    fi
 }
 
 trap_to_dfu() {
@@ -24,7 +43,7 @@ trap_to_dfu() {
 
     # Loop to read from 101 so that it stays on DFU mode afterwards.
     counter=0
-    until $DFU -a 4 -U $dfu_lock > /dev/null 2>&1
+    until $dfu_cmd -a 4 -U $dfu_lock > /dev/null 2>&1
     do
         sleep 0.1
     
@@ -41,18 +60,18 @@ trap_to_dfu() {
 }
 
 upload() {
-    $DFU -a 7 -R -D $payload_bin
-    echo "Sketch will execute in about 5 seconds."
+    eval $dfu_download || error_out "ERROR: DFU transfer failed"
+    echo "SUCCESS: Sketch will execute in about 5 seconds."
 }
 
 main() {
     echo "Starting upload script"
     setup "$@"
 
-    echo "Waiting for device... "
+    dbg_print "Waiting for device... "
     trap_to_dfu
 
-    echo "Using dfu-util to send " $payload_bin
+    dbg_print "Using dfu-util to send " $payload_bin
     upload
 
     exit 0
