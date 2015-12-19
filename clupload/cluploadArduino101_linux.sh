76bc322c2ef70501b53887de1ad682f3f603fa2c
Krzysztof Sywula <krzysztof.m.sywula@intel.com>
2015-12-18 17:47:56 -0800

    Handling verbose option in upload script
    JIRA: ATLEDGE-453

--- clupload/cluploadArduino101_linux.sh
+++ clupload/cluploadArduino101_linux.sh
@@ -1,41 +1,60 @@
 #!/bin/sh
 
-echo "starting download script"
-echo "Args to shell:" $*
-#
-# ARG 1: Path to lsz executable.
-# ARG 2: Elf File to download
-# ARG 3: TTY port to use.
-#
-#path may contain \ need to change all to /
-path_to_exe=$1
-fixed_path=${path_to_exe//\\/\/}
+dbg_print() {
+	if [ "x$verbosity" == "xverbose" ]; then
+		echo "$@"
+	fi
+}
 
-#
-tty_port_id=$3
-echo "Serial Port PORT" $com_port_id 
+error_out() {
+	echo "$@"
+	exit 1
+}
 
-#Download the file.
-host_file_name=$2
-bin_file_name=${host_file_name/elf/bin}
-echo "BIN FILE" $bin_file_name
+main() {
+	echo "Starting download script..."
 
+	# ARG 1: Path to additional executables.
+	# ARG 2: Elf File to download
+	# ARG 3: TTY port to use.
+	# ARG 4: quiet/verbose
 
-DFU="$fixed_path/dfu-util -d,8087:0ABA"
-echo "wating for Arduino 101 device... "
-COUNTER=0
-f=`$DFU -l | grep sensor_core | cut -f 1 -d ' '`
-while [ "x$f" = "x" ] && [ $COUNTER -lt 10 ]
-do
-    let COUNTER=COUNTER+1
-    sleep 1
-    f=`$DFU -l | grep sensor_core | cut -f 1 -d ' '`
-done
+	fixed_path="$1"
+	# Download .bin file instead of provided .elf
+	bin_file_name=${2/.elf/.bin}
+	com_port="$3"
+	verbosity="$4"
+	dfu="$fixed_path/dfu-util"
+	dfu_cmd="$dfu -d,8087:0ABA"
 
-if [ "x$f" != "x" ] ; then
-	echo "Using dfu-util to send " $bin_file_name
-	$DFU -D $bin_file_name -v --alt 7 -R
-	echo "Sketch will execute in about 5 seconds."
-else
-	echo "ERROR: Timed out waiting for Arduino 101."
-fi
+	dbg_print "Args to shell:" "$@"
+	dbg_print "Serial Port:" "$com_port"
+	dbg_print "BIN FILE" "$bin_file_name"
+	dbg_print "Wating for Arduino 101 device... "
+
+	COUNTER=0
+	f=$($dfu_cmd -l | grep sensor_core | cut -f 1 -d ' ')
+	while [ "x$f" = "x" ] && [ $COUNTER -lt 10 ]
+	do
+		let COUNTER=COUNTER+1
+		sleep 1
+		f=$($dfu_cmd -l | grep sensor_core | cut -f 1 -d ' ')
+	done
+
+	if [ "x$verbosity" == "xverbose" ] ; then
+		dfu_download="$dfu_cmd -D $bin_file_name -v --alt 7 -R"
+	else
+		dfu_download="$dfu_cmd -D $bin_file_name --alt 7 -R >/dev/null 2>&1"
+	fi
+
+	if [ "x$f" != "x" ] ; then
+		dbg_print "Using dfu-util to send " "$bin_file_name"
+		dbg_print "$dfu_download"
+		eval $dfu_download || error_out "ERROR: DFU transfer failed"
+		echo "SUCCESS: Sketch will execute in about 5 seconds."
+	else
+		error_out "ERROR: Timed out waiting for Arduino 101."
+	fi
+}
+
+main "$@"
